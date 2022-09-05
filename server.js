8c52a2c85799b0fa39d491c56a6e6c3c12ed97d3
ahmadjubran <ahmadjubran.98@hotmail.com>
2022-09-05 15:20:43 +0300

    Test the server

---
+++ server.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.post("/person", (req, res) => {
+  const { person } = req.body;
+
+  res
+    .status(200)
+    .json({ name: person.name, age: person.age + 5, gender: person.gender });
+});
+
+const start = (port) => {
+  app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+  });
+};
+
+module.exports = {
+  app: app,
+  start: start,
+};
